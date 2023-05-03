28e8dd21c5a952cc92b06a467315badaafa19031
nsdYuri <yurichichedom808@gmail.com>
2023-05-03 19:35:14 -0300

    atualizando projeto

---
+++ js/script.js
@@ -0,0 +1,24 @@
+"use strict";
+// console.log("Olá Mundo!!");
+
+// var nr1 = "10";
+// var nr2 = "5";
+
+// //Realizando operações com Strings e realizando o parse com a
+// //função parseInt...;
+// console.log(parseInt(nr1)+parseInt(nr2));
+
+/*var nome1 = "Dubas";
+// let nome2 = "Serbatinho";
+// const nome3 ="Julia";
+nome = "Ale";
+console.log(nome);
+
+if(nome1 != ""){
+    let nome1 = "Julia";
+}
+
+var nome
+
+console.log(nome1);
+*/
